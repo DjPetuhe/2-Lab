28a3a0c31a0e9c7cc960b633715f136c6aa6f7da
Valid <validhernufKPI@gmail.com>
2021-02-28 02:40:57 +0200

    Entering file directories

---
+++ Head.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*Шаблонная функция для получения определенного значения.
+я оставил её в хедере, потому что в противном случае возникают проблемы с template*/
+
+template <typename T>
+void input(string str, T *a)
+{
+    cout << str;
+    cin >> *a;
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <string>
+#include "Head.h"
 using namespace std;
 
 int main()
 {
+    //Ввод кол-ва файлов, и проврека на то, чтобы значения были приемлимыми.
+    int amount_of_files;
+    input("How many files do you want to work with?: ", &amount_of_files);
+    if (amount_of_files <= 0 || amount_of_files >= 100)
+    {
+        cout << "Wrong value.";
+        return 0;
+    }
+    //Ввод директории файлов в массив стрингов.
+    string file_directories[amount_of_files];
+    for (int i = 0; i < amount_of_files; i++)
+    {
+        input("Enter the file directory: ", &file_directories[i]);
+    }
+    return 0;
 }
